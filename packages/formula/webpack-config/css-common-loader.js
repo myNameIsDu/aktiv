bd5c171dbb53759fb5a17095f56d11efa4f0d6fe
Jenkin <jiancunge.com@gmail.com>
2021-07-27 01:11:57 +0800

    chore: format code

--- packages/formula/webpack-config/css-common-loader.js
+++ packages/formula/webpack-config/css-common-loader.js
@@ -1,27 +1,25 @@
-const NODE_ENV = process.env.NODE_ENV;
+const { NODE_ENV } = process.env;
 const MintCssExtractWebpackPlugin = require('mini-css-extract-plugin');
-
 
 const isDevelopment = NODE_ENV === 'development';
 const isProduction = NODE_ENV === 'production';
+
 module.exports = () => {
     return [
         isDevelopment && 'style-loader',
         isProduction && {
             loader: MintCssExtractWebpackPlugin.loader,
             options: {
-                publicPath: '../'
-            }
+                publicPath: '../',
+            },
         },
         'css-loader',
         isProduction && {
             loader: 'postcss-loader',
             options: {
                 postcssOptions: {
-                    plugins: [
-                        'postcss-preset-env',
-                    ],
-                }
+                    plugins: ['postcss-preset-env'],
+                },
             },
         },
     ].filter(Boolean);
