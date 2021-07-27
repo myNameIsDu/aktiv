9761c0fda3995d52a3961de1552877c9a4dc2fe3
du <sunbohaoddd@gmail.com>
2021-07-27 23:43:33 +0800

    style: import & const add space

--- packages/launcher/rollup.config.js
+++ packages/launcher/rollup.config.js
@@ -3,6 +3,7 @@ import tsc from '@rollup/plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+
 const extensions = ['js', 'jsx', 'ts', 'tsx'];
 
 export default {
