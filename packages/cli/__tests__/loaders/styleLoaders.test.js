6b0c4794fb4e073e35dfe9cdd750cbf6ca1810a5
du <sunbohaoddd@gmail.com>
2021-11-29 01:46:16 +0800

    fix: 删除styleloaders快照,改为matchObject

--- packages/cli/__tests__/loaders/styleLoaders.test.js
+++ packages/cli/__tests__/loaders/styleLoaders.test.js
@@ -1,17 +1,70 @@
 const styleLoaders = require('../../ak-webpack-config/lib/loaders/styleLoaders');
 
 describe('styleLoaders快照', () => {
+    const testResult = extractCSS => [
+        {
+            test: /\.css$/,
+            use: [
+                extractCSS
+                    ? {
+                          loader: expect.stringMatching(
+                              'node_modules/mini-css-extract-plugin/dist/loader.js',
+                          ),
+                      }
+                    : expect.stringMatching('node_modules/style-loader/dist/cjs.js'),
+                expect.stringMatching('node_modules/css-loader/dist/cjs.js'),
+                {
+                    loader: expect.stringMatching('node_modules/postcss-loader/dist/cjs.js'),
+                    options: {
+                        postcssOptions: {
+                            plugins: [expect.any(Function)],
+                        },
+                    },
+                },
+            ],
+        },
+        {
+            test: /\.less$/,
+            use: [
+                extractCSS
+                    ? {
+                          loader: expect.stringMatching(
+                              'node_modules/mini-css-extract-plugin/dist/loader.js',
+                          ),
+                      }
+                    : expect.stringMatching('node_modules/style-loader/dist/cjs.js'),
+                expect.stringMatching('node_modules/css-loader/dist/cjs.js'),
+                {
+                    loader: expect.stringMatching('node_modules/postcss-loader/dist/cjs.js'),
+                    options: {
+                        postcssOptions: {
+                            plugins: [expect.any(Function)],
+                        },
+                    },
+                },
+                {
+                    loader: expect.stringMatching('node_modules/less-loader/dist/cjs.js'),
+                    options: {
+                        lessOptions: {
+                            javascriptEnabled: true,
+                        },
+                    },
+                },
+            ],
+        },
+    ];
+
     it('extractCSS为false', () => {
         // @ts-ignore 仅使用到extractCss
         const loaders = styleLoaders({ extractCSS: false });
 
-        expect(loaders).toMatchSnapshot();
+        expect(loaders).toMatchObject(testResult(false));
     });
 
     it('extractCSS为true', () => {
         // @ts-ignore 仅使用到extractCss
         const loaders = styleLoaders({ extractCSS: true });
 
-        expect(loaders).toMatchSnapshot();
+        expect(loaders).toMatchObject(testResult(true));
     });
 });
