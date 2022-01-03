0a016f5757271e2ddbdf9ad1bf050dd0aabd54cc
eric_wang <jadeface.scholar@gmail.com>
2022-01-03 21:58:33 +0800

    feat: discard useQuery, anyone can use useSearchParams

--- packages/launcher/src/hooks/index.ts
+++ packages/launcher/src/hooks/index.ts
@@ -1,6 +1,5 @@
 import useRouter from './use-router';
 import type { UseRouterReturns } from './use-router';
-import useQuery from './use-query';
 
-export { useRouter, useQuery };
+export { useRouter };
 export type { UseRouterReturns };
