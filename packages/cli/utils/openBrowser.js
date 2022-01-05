0a35d400323b2c8231515ede4fcbecbfb78519b8
eric_wang <jadeface.scholar@gmail.com>
2022-01-05 20:43:23 +0800

    perf: perf cli openBrowser in wsl env with xdg-open

--- packages/cli/utils/openBrowser.js
+++ packages/cli/utils/openBrowser.js
@@ -1,5 +1,7 @@
-const { execSync } = require('child_process');
+const { execSync, spawn } = require('child_process');
 const open = require('open');
+const isWsl = require('is-wsl');
+const chalk = require('chalk');
 
 /**
  * @param {string} url url
@@ -39,6 +41,35 @@ function startBrowserProcess(url) {
             }
         }
     }
+
+    if (isWsl) {
+        const cliArguments = [url];
+        const command = 'xdg-open';
+
+        try {
+            const subprocess = spawn(command, cliArguments);
+
+            subprocess.unref();
+
+            return true;
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log(
+                chalk.red(`
+            Start Error:
+                 Aktiv Cli detected that you have not installed ${chalk.underline.bgBlue(
+                     'xdg-open',
+                 )},You can try to ${chalk.underline.bgBlue(
+                    'sudo apt-get install xdg-open',
+                )}  to install and then start again
+            `),
+            );
+            // Don't continue
+
+            return false;
+        }
+    }
+
     try {
         // eslint-disable-next-line @typescript-eslint/no-empty-function
         open(url).catch(() => {}); // Prevent `unhandledRejection` error.
