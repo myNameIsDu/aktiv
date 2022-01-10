d04857d72b67a0c0a9d22a8d40a426853a9a1661
eric_wang <jadeface.scholar@gmail.com>
2022-01-10 21:50:54 +0800

    style: change typo

--- packages/cli/config/index.js
+++ packages/cli/config/index.js
@@ -1,6 +1,6 @@
 const defaultWorkDir = process.cwd(); // 当前工作目录
 const defaultConfigFile = './ak.config.js'; // 配置文件路径
-const defaultProt = 8080; // 默认端口
+const defaultPort = 8080; // 默认端口
 const devBuildEnv = 'development';
 const proBuildEnv = 'production';
 const localBuildEnv = 'local';
@@ -18,7 +18,7 @@ const supperEnvList = [localBuildEnv, devBuildEnv, proBuildEnv];
 module.exports = {
     defaultWorkDir,
     defaultConfigFile,
-    defaultProt,
+    defaultPort,
     localBuildEnv,
     devBuildEnv,
     proBuildEnv,
