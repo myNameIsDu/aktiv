56010bb1adc3f4d63e0e52a9cc8acfbf36a3d0e4
myNameIsDu <sunbohaoddd@gmail.com>
2022-01-19 11:26:50 +0800

    chore(root): 增加commitlint scope enum, scope不能为空

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [2, 'always', ['cli', 'launcher', 'root']],
+        'scope-empty': [2, 'never'],
+    },
+};
