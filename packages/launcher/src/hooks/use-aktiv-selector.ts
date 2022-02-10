199017b10d0923854a281668b30e199972c6b293
eric_wang <jadeface.scholar@gmail.com>
2022-02-10 21:06:48 +0800

    perf(launcher): perf code

--- packages/launcher/src/hooks/use-aktiv-selector.ts
+++ packages/launcher/src/hooks/use-aktiv-selector.ts
@@ -1,32 +1,35 @@
-import { useRef } from 'react';
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { isString } from '../utils/isType';
 import { get } from '../utils/get';
 
-export type OriginSelectorType = Parameters<typeof useSelector>[0];
+export type OriginSelectorType<S, T> = (state: S) => T;
 
-export type OriginEqualFnType = Parameters<typeof useSelector>[1];
+export type OriginEqualFnType<T> = (left: T, right: T) => boolean;
 
-export type SelectorTypes = OriginSelectorType | string;
+export type SelectorTypes<S, T> = OriginSelectorType<S, T> | string;
 
-function useAktivSelector<T>(selector: SelectorTypes, equalityFn?: OriginEqualFnType): T {
-    const { current: reduxSelector } = useRef<() => typeof selector>(() => {
+function useAktivSelector<S extends Record<string, unknown>, T = unknown>(
+    selector: SelectorTypes<S, T>,
+    equalityFn?: OriginEqualFnType<T>,
+): T {
+    const reduxSelector = useMemo<OriginSelectorType<S, T>>(() => {
         if (typeof selector === 'function') {
             return selector;
         }
 
-        return state => {
+        return (state: S): T => {
             const [key, ...path] = isString(selector) ? (selector as string).split('.') : selector;
 
             if (path.length) {
-                return get((state as Record<string, unknown>)[key], path);
+                return get(state[key], path) as any;
             }
 
-            return (state as Record<string, unknown>)[key];
+            return state[key] as any;
         };
-    });
+    }, [selector]);
 
-    return useSelector(reduxSelector() as OriginSelectorType, equalityFn) as T;
+    return useSelector<S, T>(reduxSelector, equalityFn);
 }
 
 export default useAktivSelector;
