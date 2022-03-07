fc78212e0e068470c3107b160b8e2ffc19e1ffbf
eric_wang <jadeface.scholar@gmail.com>
2022-03-07 23:04:50 +0800

    feat(launcher): add use-effect-router

--- packages/launcher/src/hooks/index.ts
+++ packages/launcher/src/hooks/index.ts
@@ -1,13 +1,17 @@
 import useRouter from './use-router';
 import useAktivSelector from './use-aktiv-selector';
+import useEffectRouter from './use-effect-router';
 import type { UseRouterReturns, UseRouterState } from './use-router';
 import type { OriginSelectorType, OriginEqualFnType, SelectorTypes } from './use-aktiv-selector';
+import type { SearchProps, UseEffectRouterReturns } from './use-effect-router';
 
-export { useRouter, useAktivSelector };
+export { useRouter, useAktivSelector, useEffectRouter };
 export type {
     UseRouterReturns,
     OriginSelectorType,
     OriginEqualFnType,
     SelectorTypes,
     UseRouterState,
+    SearchProps,
+    UseEffectRouterReturns,
 };
---
+++ packages/launcher/src/hooks/use-effect-router.ts
@@ -0,0 +1,48 @@
+import { useCallback, useMemo } from 'react';
+import { useSearchParams, useLocation } from 'react-router-dom';
+import useRouter from './use-router';
+
+export type SearchProps = Partial<Record<string, unknown>>;
+
+export type UseEffectRouterReturns = {
+    redirect(search?: SearchProps): void;
+    replace(search?: SearchProps): void;
+};
+
+const useEffectRouter = (): UseEffectRouterReturns => {
+    const { pathname, state } = useLocation();
+    const router = useRouter();
+    const params = useSearchParams();
+
+    const redirect = useCallback(
+        search => {
+            router.redirect(pathname, {
+                state,
+                ...params,
+                ...search,
+            });
+        },
+        [params, pathname, router, state],
+    );
+
+    const replace = useCallback(
+        search => {
+            router.replace(pathname, {
+                state,
+                ...params,
+                ...search,
+            });
+        },
+        [params, pathname, router, state],
+    );
+
+    return useMemo(
+        () => ({
+            redirect,
+            replace,
+        }),
+        [redirect, replace],
+    );
+};
+
+export default useEffectRouter;
