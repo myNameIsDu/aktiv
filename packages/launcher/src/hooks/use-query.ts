17357b0853bd25a19055676e29057fbcd30d206c
eric_wang <jadeface.scholar@gmail.com>
2022-04-25 23:39:09 +0800

    feat(launcher): handle review faq

--- packages/launcher/src/hooks/use-query.ts
+++ packages/launcher/src/hooks/use-query.ts
@@ -3,11 +3,11 @@ import qs from 'qs';
 import { useLocation } from 'react-router-dom';
 
 const useQuery = (): Record<string, any> => {
-    const location = useLocation();
+    const { search } = useLocation();
 
     return useMemo(() => {
-        return qs.parse(location?.search, { ignoreQueryPrefix: true });
-    }, [location?.search]);
+        return qs.parse(search, { ignoreQueryPrefix: true });
+    }, [search]);
 };
 
 export default useQuery;
