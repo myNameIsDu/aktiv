35b70768de671d9f6c69d9fc4b80f13a46b9eafd
myNameIsDu <sunbohaoddd@gmail.com>
2022-07-10 18:28:35 +0800

    chore(cli): fix jest.config.js

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     preset: 'ts-jest',
     collectCoverage: true,
     coverageDirectory: 'coverage',
-    collectCoverageFrom: ['./utils/**', './ak-webpack-config/**'],
+    collectCoverageFrom: ['<rootDir>/utils/**.{js,jsx}', '<rootDir>/ak-webpack-config/.{js,jsx}'],
     // setupFilesAfterEnv: ['./__tests__/jest.setup.ts'],
     transform: {
         '^.+\\.(t|j)sx?$': ['@swc/jest'],
